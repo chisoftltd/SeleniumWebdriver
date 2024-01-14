bcf19e51cbebb5dc387676ffcddda2642e4be3ac
chisoftltd <chisoftltd@gmail.com>
2024-01-14 23:26:35 +0000

    Using Selenium to enter first name, last name and email

--- interaction.py
+++ interaction.py
@@ -8,13 +8,17 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.nairaland.com/search?q=&search=Search")
+driver.get("http://secure-retreat-92358.herokuapp.com/")
 
 # all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
 # all_portals.click()
 
-search = driver.find_element(By.NAME, value="q")
-search.send_keys("Tinubu", Keys.ENTER)
+search_f = driver.find_element(By.NAME, value="fName")
+search_f.send_keys("Benjamin")
+search_l = driver.find_element(By.NAME, value="lName")
+search_l.send_keys("Chinwe")
+search_e = driver.find_element(By.NAME, value="email")
+search_e.send_keys("Chinwe@kth.se", Keys.ENTER)
 
 # article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 # print(article_count.text)
