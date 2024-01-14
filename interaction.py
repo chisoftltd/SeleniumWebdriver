28b454ec04ab8fafe8ae98e7e1c98a9b96f80d5d
chisoftltd <chisoftltd@gmail.com>
2024-01-14 23:16:53 +0000

    Using Selenium to search website

--- interaction.py
+++ interaction.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 # Keep Chrome browser open after program finishes
@@ -7,11 +8,15 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://www.nairaland.com/search?q=&search=Search")
 
-article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(article_count.text)
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
 
+search = driver.find_element(By.NAME, value="q")
+search.send_keys("Tinubu", Keys.ENTER)
 
+# article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+# print(article_count.text)
+# article_count.click()
 
-driver.quit()
